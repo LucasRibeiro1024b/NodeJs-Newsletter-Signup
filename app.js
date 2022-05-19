090e9c2591b0b2612819a7090cb3c92edda8c63d
Lucas Ribeiro <lucasribeiro1024b@gmail.com>
2022-05-18 21:41:34 -0300

    work

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require("request");
+const { sendFile } = require('express/lib/response');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res)=>{
+  
+});
 
 app.listen(3000, function(){
   console.log("Server running in port 3000");
